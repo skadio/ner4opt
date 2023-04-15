896503fc8a7a4612f51dd39794bbb6096a07e14d
Karthik Uppuluri <karthik.uppuluri@fmr.com>
2023-04-15 19:23:09 -0400

    Removed example usage and help and moved it to Ner4Opt class

--- ner4opt/__init__.py
+++ ner4opt/__init__.py
@@ -3,19 +3,6 @@
 This module defines the public interface of the
 **Ner4Opt Library** providing access to the following modules:
     - ``Ner4Opt``
-
-Example Usage
-~~~~~~~~~~~~~
-
->>> from ner4opt import Ner4Opt
->>> example_problem = "How much should the firm allocate in each asset so as to maximize its average return ?"
->>> ner4opt = Ner4Opt('hybrid')  # select a model
->>> entities = ner4opt.get_entities(example_problem)  # get entities
-
-Help
-~~~~
->>> from ner4opt import Ner4Opt
->>> Ner4Opt.__doc__
 """
 
 from ner4opt._version import __version__
