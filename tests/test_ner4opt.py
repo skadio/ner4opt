0c767e5f9cc3605bca650559acb07aeb962c6307
Karthik Uppuluri <karthik.uppuluri@fmr.com>
2023-04-15 19:26:09 -0400

    Added comments for easy understanding of both valid usage and invalid usage tests

--- tests/test_ner4opt.py
+++ tests/test_ner4opt.py
@@ -40,8 +40,10 @@ class Ner4OptTest(unittest.TestCase):
 
         _current_dir = Path(__file__).parent
 
+        # Example Problem Description
         self._example = self.read_example_sentence(os.path.join(_current_dir, "fixtures/example_problem.txt"))
 
+        # Expected Results for each model for the above problem description
         self._expected_lexical_result = self.load_pickled_object(
             os.path.join(_current_dir, "fixtures/lexical_result.json"))
         self._expected_lexical_plus_result = self.load_pickled_object(
@@ -51,11 +53,13 @@ class Ner4OptTest(unittest.TestCase):
         self._expected_hybrid_result = self.load_pickled_object(
             os.path.join(_current_dir, "fixtures/hybrid_result.json"))
 
+        # Ner4opt Objects for each model
         self._lexical_ner_model = Ner4Opt("lexical")
         self._lexical_plus_ner_model = Ner4Opt("lexical_plus")
         self._semantic_ner_model = Ner4Opt("semantic")
         self._hybrid_ner_model = Ner4Opt("hybrid")
 
+    # Usage tests for all models
     def test_get_entities_lexical(self):
         result = self._lexical_ner_model.get_entities(self._example)
         self.assert_list_of_dicts_equal(result, self._expected_lexical_result)
@@ -72,6 +76,7 @@ class Ner4OptTest(unittest.TestCase):
         result = self._hybrid_ner_model.get_entities(self._example)
         self.assert_list_of_dicts_equal(result, self._expected_hybrid_result)
 
+    # Invalid usage test
     def test_get_entities_invalid_usage(self):
 
         with self.assertRaises(InvalidModelError) as context:
